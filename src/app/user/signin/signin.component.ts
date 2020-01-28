5b405039cf3dc57804a0cdf7d94659870fdc2e8a
Dankhra sandip <39943347+SandipDankhra@users.noreply.github.com>
2020-01-28 19:19:16 +0530

    commite

--- src/app/user/signin/signin.component.ts
+++ src/app/user/signin/signin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Route } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  userFormGroup: FormGroup;
+  constructor(private formBilder: FormBuilder, private route: Route) { }
 
   ngOnInit() {
   }
+  userJson: any = [
+    {
+      "id": 1,
+      "firstName": "Sandip",
+      "lastName": "Patel",
+      "emailAddress": "sandip@gmail.com",
+      "password": "12345"
+    },
+    {
+      "id": 2,
+      "firstName": "Vikas",
+      "lastName": "Patel",
+      "emailAddress": "vikas@gmail.com",
+      "password": "12345"
+    },
+    {
+      "id": 3,
+      "firstName": "Yagnik",
+      "lastName": "patel",
+      "emailAddress": "Yagnik@gmail.com",
+      "password": "12345"
+    }
+  ];
 
+  signIn() {
+    for (var i = 0; i < this.userJson.length; i++) {
+      console.log(this.userJson[i]);
+    }
+  }
 }
