e4f9cf8df034f5a4479178534a0bbed2d506903d
Dankhra sandip <39943347+SandipDankhra@users.noreply.github.com>
2020-02-03 12:16:29 +0530

    Edit Complete

--- src/app/user/user-edit/user-edit.component.ts
+++ src/app/user/user-edit/user-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { Router ,ActivatedRoute} from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-edit',
@@ -11,17 +11,16 @@ export class UserEditComponent implements OnInit {
 
   userEditFormGroup: FormGroup;
   id;
-  constructor(private formBuilder: FormBuilder, private route: Router, private activatedRoute:ActivatedRoute) { }
+  constructor(private formBuilder: FormBuilder, private route: Router, private activatedRoute: ActivatedRoute) { }
 
   userData: any = [
     { id: 1, firstname: "Sandip", lastname: "Dankhra", Email: "sandip@gmail.com", password: "1111" },
     { id: 2, firstname: "Vikas", lastname: "Patel", Email: "vikas@gmail.com", password: "1111" },
   ]
   ngOnInit() {
-   this.id =  this.activatedRoute.snapshot.paramMap.get("id");
+    this.id = this.activatedRoute.snapshot.paramMap.get("id");
 
     this.userEditFormGroup = this.formBuilder.group({
-      
       firstName: [''],
       lastName: [''],
       email: [''],
@@ -30,15 +29,20 @@ export class UserEditComponent implements OnInit {
   }
 
   userEditData() {
+
     console.log(this.id);
-    
-    this.userData[this.id] = ({
-      "id": (parseInt(this.userData.id)).toString(),
-      firstname: this.userEditFormGroup.controls.firstName.value,
-      lastname: this.userEditFormGroup.controls.lastName.value,
-      Email: this.userEditFormGroup.controls.email.value,
-      password: this.userEditFormGroup.controls.passWord.value,
-    });
+
+    for (var i = 0; i <= this.userData[0].id == this.id; i++) {
+      
+      
+    }
+    // this.userData[this.id] = ({
+    //   "id": (parseInt(this.userData.id)).toString(),
+    //   firstname: this.userEditFormGroup.controls.firstName.value,
+    //   lastname: this.userEditFormGroup.controls.lastName.value,
+    //   Email: this.userEditFormGroup.controls.email.value,
+    //   password: this.userEditFormGroup.controls.passWord.value,
+    // });
 
     console.log(this.userEditFormGroup.value);
   }
